50208f907d1ec7ac1720fef2b8c4d37246c4bca2
fmthoker <fmthoker@gmail.com>
2022-08-02 01:05:08 +0200

    test run finegym actions

--- action_recognition/datasets/gym_288.py
+++ action_recognition/datasets/gym_288.py
@@ -8,7 +8,7 @@
 from datasets.video_db import VideoDataset
 import os
 DATA_PATH = '/local-ssd/fmthoker/gym/videos'
-ANNO_PATH = '/ssdstore/fmthoker/gym/annotations'
+ANNO_PATH = '/local-ssd/fmthoker/gym/annotations'
 
 
 class GYM288(VideoDataset):
